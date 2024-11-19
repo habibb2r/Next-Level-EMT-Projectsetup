bcb249b9d20342f5b73a864e6b2c2965c3cf6b1c
habibb2r <habibhk127@gmail.com>
2024-11-19 13:26:53 +0600

    Completed Setup

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 const app: Application = express();
 
-const test = 30;
-let data = 2;
+
 //parser
 
 app.use(express.json());
--- src/server.ts
+++ src/server.ts
@@ -12,5 +12,6 @@ async function main() {
     console.error(err);
   }
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+main()
